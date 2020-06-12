c8ce91caac0d965f0d119c53bb48bb1472cbafb9
Erich Eichinger <eeichinger@gmail.com>
2020-06-12 23:48:15 +0200

    include oled display

--- funicular_controller_v2.ino
+++ funicular_controller_v2.ino
@@ -3,13 +3,28 @@
 
 
 */
-
-#include <LegoHallRotEncoder.h>
-#include <Wire.h>
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_PN532.h>
 #include <L293.h>
+#include <LegoHallRotEncoder.h>
 
+#define DEBUG
+
+/**
+    DISPLAY
+*/
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+/**
+    Motor
+*/
 // give a name to the pins that we use
 const int speedPin = 6;    // that is the pin that we use to control the motor's speed
 const int forwardPin = 7; // this is the pin that we use to tell the motor to go forward
@@ -17,6 +32,9 @@ const int reversePin = 8; // this is the pin that we use to tell the motor to go
 
 L293 motor( speedPin, forwardPin, reversePin );
 
+/**
+    NFC Sensors
+*/
 // If using the breakout with SPI, define the pins for SPI communication.
 //#define PN532_SCK  (13)
 //#define PN532_MISO (12)
@@ -41,12 +59,25 @@ const uint16_t carIds[2][2] = {
 Adafruit_PN532 nfc_beforeStation(PN532_SS);
 Adafruit_PN532 nfc_inStation(PN532_SS_2);
 
+/**
+    Rotary Encoder sensing rope speed + dir
+*/
 // create LegoHallRotEncoder
 LegoHallRotEncoder rotEnc = LegoHallRotEncoder(PIN_HALL1, PIN_HALL2);
 
-ISR(PCINT2_vect) {
-  static byte oldRotEncState = 0x0;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+static void nothing(void) {
+}
+
+void (*cb)(void) = nothing;
+
+ISR(PCINT2_vect) {
+  cb();
+}
+
+static void myISR() {
+  static byte oldRotEncState = 0x0;
   // see https://arduino.stackexchange.com/a/12958 how to handle pin change interrupts
   byte newRotEncState = PIND & 0b00110000;  //(bit(4)|bit(5));
   if (newRotEncState != oldRotEncState) {
@@ -56,18 +87,18 @@ ISR(PCINT2_vect) {
 }
 
 void initRFIDReader(Adafruit_PN532& rfidReader, String name, uint8_t irq_pin) {
-  Serial.print("Init PN532 Reader:"); Serial.println(name);
+  Serial.print(F("Init PN532 Reader:")); Serial.println(name);
   rfidReader.begin();
   uint32_t versiondata;
   versiondata = rfidReader.getFirmwareVersion();
   if (! versiondata) {
-    Serial.print("Didn't find PN53x board InStation");
+    Serial.print(F("Didn't find PN53x board InStation"));
     while (1); // halt
   }
   // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
-  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
-  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
+  Serial.print(F("Found chip PN5")); Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print(F("Firmware ver. ")); Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print(F(".")); Serial.println((versiondata >> 8) & 0xFF, DEC);
   // configure board to read RFID tags
   rfidReader.SAMConfig();
 
@@ -78,37 +109,95 @@ void initRFIDReader(Adafruit_PN532& rfidReader, String name, uint8_t irq_pin) {
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(115200);
+  Serial.println(F("BEGIN"));
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    Serial.flush();
+    for (;;); // Don't proceed, loop forever
+  }
+
+  motor.forceStop(200); // stop the motor
+
+  initRFIDReader(nfc_beforeStation, "BeforeStation", PN532_IRQ);
+  initRFIDReader(nfc_inStation, "InStation", PN532_IRQ_2);
+
+  cb = myISR;
+
   cli(); // disable interrupts
   PCICR |= 0b00000100; // turn on pin change irq on port d
   PCMSK2 |= 0b00110000;    // turn on pins PD4 & PD5, PCINT20 & PCINT21 (=Arduino pins D4, D5)
   sei(); // reenable interrupts
 
-  Serial.begin(115200);
-  motor.forceStop(200); // stop the motor
-  
-  initRFIDReader(nfc_beforeStation, "BeforeStation", PN532_IRQ);
-  initRFIDReader(nfc_inStation, "InStation", PN532_IRQ_2);
+//   Show initial display buffer contents on the screen --
+//   the library initializes this with an Adafruit splash screen.
+  display.display();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK); // Draw white text
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  Serial.println(F("setup complete"));
 }
+
+static uint32_t time = millis();
+
+void testprint(int counter, int8_t car_inStation, int8_t car_beforeStation) {
+  display.clearDisplay();
+
+  display.setCursor(0, 0);     // Start at top-left corner
+//  display.println("text-performance test");
+  //  for (int i = 0; i < 255; i++) {
+  ////    display.setCursor(0, 9);
+  ////    display.print("A");
+  //    display.display();
+  //
+  //  }
+  time = millis() - time;
+  display.setCursor(0, 9);
+  display.print(F("loop time:"));
+  display.print(time);
+  display.print("ms");
+
+  display.setCursor(0, 18);
+  display.print(F("counter:"));
+  display.print(counter);
+  
+  display.setCursor(0, 27);
+  display.print(F("car_inStation:"));
+  display.print(car_inStation);
+
+  display.setCursor(0, 36);
+  display.print(F("car_beforeStation:"));
+  display.print(car_beforeStation);
+
+  display.display();
+
+  time = millis();
+  //  delay(2000);
+}
+
+static byte uid[] = "\0\0\0\0\0\0\0"; // Buffer to store the returned UID
+static byte uidLength;                // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
 int8_t detectCar(Adafruit_PN532& nfc) { // carid = -1, 0=no car, 1
   byte success = false;
-  byte uid[] = "\0\0\0\0\0\0\0"; // Buffer to store the returned UID
-  byte uidLength;                // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
   // fetch result if IRQ line indicates that there is one
   success = nfc.completeReadPassiveTargetID(uid, &uidLength);
   int8_t car_id = 0;
   if (success) {
     uint16_t curcar =  (uid[1] << 8) | uid[2];
+#ifdef DEBUG
     Serial.print("car id:");
     Serial.print(curcar, HEX);
     Serial.println("");
-
-//    Serial.print(carIds[0][0], HEX);
-//    Serial.print(carIds[0][1], HEX);
+#endif
+    //    Serial.print(carIds[0][0], HEX);
+    //    Serial.print(carIds[0][1], HEX);
     //    Serial.print(carIds[1][0], HEX);
     //    Serial.print(carIds[1][1], HEX);
-//    Serial.println("");
+    //    Serial.println("");
     if ( carIds[0][0] == curcar || carIds[0][1] == curcar ) {
       car_id = -1;
     }
@@ -124,24 +213,32 @@ int8_t detectCar(Adafruit_PN532& nfc) { // carid = -1, 0=no car, 1
 
 // the loop function runs over and over again forever
 void loop() {
-  rotEnc.printDebug();
-//  Serial.print("dir:");
-//  Serial.print(rotEnc.dir());
-//  Serial.print("counter:");
-//  Serial.print(rotEnc.counter());
-//  Serial.println();
+#ifdef DEBUG
+    rotEnc.printDebug();
+  //  Serial.print("dir:");
+  //  Serial.print(rotEnc.dir());
+  //  Serial.print("counter:");
+  //  Serial.print(rotEnc.counter());
+  //  Serial.println();
+#endif    
 
   if (rotEnc.counter() > 50) rotEnc.resetCounter();
 
   int8_t car_inStation = detectCar(nfc_inStation);
   if (car_inStation != 0) {
+#ifdef DEBUG
     Serial.print("car inStation:"); Serial.println(car_inStation);
+#endif  
   }
   int8_t car_beforeStation = detectCar(nfc_beforeStation);
   if (car_beforeStation != 0) {
+#ifdef DEBUG
     Serial.print("car beforeStation:"); Serial.println(car_beforeStation);
+#endif  
   }
 
+#ifdef DEBUG
   Serial.flush();
-  delay(50);                       // wait a bit
+#endif  
+  testprint(rotEnc.counter(), car_inStation, car_beforeStation);
 }
