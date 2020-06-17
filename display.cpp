ae26a3455675ef05216132b9e4461cddb89945fb
Erich Eichinger <eeichinger@gmail.com>
2020-06-17 20:47:42 +0200

    add uptime to display

--- display.cpp
+++ display.cpp
@@ -1,6 +1,7 @@
 #include "display.h"
 #include <SSD1306Ascii.h>
 #include <SSD1306AsciiAvrI2c.h>
+#include <math.h>
 
 //#define DEBUG
 
@@ -9,7 +10,7 @@
 */
 static SSD1306AsciiAvrI2c display;
 
-static uint32_t time = millis();
+static unsigned long time = millis();
 
 void display_init() {
   display.begin(&Adafruit128x64, 0x3C);
@@ -18,6 +19,7 @@ void display_init() {
   display.setLetterSpacing(0);
 
   display.setCursor(0, 0);
+  display.println(F("xxxxxxx uptime (s)"));
   display.println(F("xxxx refresh (ms)"));
   display.println(F("xxxx state"));
   display.println(F("xxxx speed"));
@@ -29,7 +31,7 @@ void display_init() {
 }
 
 void printPadded(int8_t val) {
-  if (val < -99) {  
+  if (val < -99) {
     // noop
   } else if (val < -9) {
     display.print(F(" "));
@@ -40,18 +42,40 @@ void printPadded(int8_t val) {
   } else if (val < 100) {
     display.print(F("  "));
   } else {
-    display.print(F(" "));    
+    display.print(F(" "));
   }
+  display.println(val);
+}
+
+const String s_spaces[] = {
+  ""
+  , " "
+  , "  "
+  , "   "
+  , "    "
+  , "     "
+  , "      "
+  , "       "
+  , "        "
+  , "         "
+  , "          "
+};
+
+void printPadded(long val, int len) {
+  int l = log10(abs(val)) + 1;
+  int spaces = max(0, len - l);
+  if (val<0 && spaces>0) spaces -=1;
+  display.print(s_spaces[spaces]);
   display.println(val);
 }
 
 void display_status(StateContext &ctx) {
   uint8_t refreshMillis = millis() - time;
   time = millis();
-  
-  
+
 #ifdef DEBUG
-  Serial.print(F("refreshMs: ")); Serial.print(refreshMillis);
+  Serial.print(F("uptime (s): ")); Serial.print(time / 1000);
+  Serial.print(F("\trefreshMs: ")); Serial.print(refreshMillis);
   Serial.print(F("\tstate: ")); Serial.print(ctx.state);
   Serial.print(F("\tspeed: ")); Serial.print(ctx.speed);
   Serial.print(F("\tdistance: ")); Serial.print(ctx.distance);
@@ -63,8 +87,10 @@ void display_status(StateContext &ctx) {
 #endif
 
   display.setCursor(0, 0);
+
+  printPadded(time/1000, 7);
   printPadded(refreshMillis);
-  printPadded(ctx.state);  
+  printPadded(ctx.state);
   printPadded(ctx.speed);
   printPadded(ctx.distance);
   printPadded(ctx.car_inStation);
